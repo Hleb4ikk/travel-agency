7a18a4e670f12c50dfaf3570c90e55c7132ba494
Hleb4ikk <hlebor2006@mail.ru>
2025-04-09 12:35:18 +0300

    Search was added

--- components/extension-ui/tours-catalog/tours-catalog.tsx
+++ components/extension-ui/tours-catalog/tours-catalog.tsx
@@ -4,16 +4,27 @@ import TourCard, { Tour } from '@/components/extension-ui/tours-catalog/tour-car
 import { useEffect, useState } from 'react';
 import { fetchTours } from '@/db/utils';
 import TourCardSkeleton from './tour-card/skeleton';
+import { useSearchParams } from 'next/navigation';
 
 export default function ToursCatalog({ callback }: { callback: (arr: Array<Tour> | null) => void }) {
   const [toursCardInfo, setToursCardInfo] = useState<Array<Tour> | null>(null);
   const [isLoading, setIsLoading] = useState(true);
+  const query = useSearchParams().get('query');
+  const [filteredTours, setFilteredTours] = useState<Array<Tour> | undefined>(undefined);
+
+  useEffect(() => {
+    setFilteredTours(toursCardInfo?.filter((tour) => tour.title.toLowerCase().includes(query?.toLowerCase() ?? '')));
+  }, [toursCardInfo, query]);
+
+  useEffect(() => {
+    callback(filteredTours ?? null);
+  }, [callback, filteredTours]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const result = await fetchTours();
         setToursCardInfo(result);
-        callback(result);
       } catch (error) {
         console.error('Failed to fetch tours:', error);
       } finally {
@@ -22,14 +33,27 @@ export default function ToursCatalog({ callback }: { callback: (arr: Array<Tour>
     };
 
     fetchData();
-  }, [callback]);
+  }, []);
 
   return (
     <section className="flex flex-wrap items-center gap-6">
-      {isLoading && <TourCardSkeleton />}
-      {toursCardInfo &&
-        toursCardInfo.length != 0 &&
-        toursCardInfo.map((tour) => (
+      {isLoading && (
+        <>
+          <TourCardSkeleton />
+          <TourCardSkeleton />
+          <TourCardSkeleton />
+          <TourCardSkeleton />
+          <TourCardSkeleton />
+          <TourCardSkeleton />
+          <TourCardSkeleton />
+          <TourCardSkeleton />
+          <TourCardSkeleton />
+          <TourCardSkeleton />
+        </>
+      )}
+      {filteredTours &&
+        filteredTours.length != 0 &&
+        filteredTours.map((tour) => (
           <TourCard
             key={tour.id}
             title={tour.title}
